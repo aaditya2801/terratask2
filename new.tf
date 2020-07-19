a96a3614931f375cdaf2845964cb12a4a8ae6c7f
aaditya2801 <66722117+aaditya2801@users.noreply.github.com>
2020-07-19 13:21:34 +0530

    Update new.tf

--- new.tf
+++ new.tf
@@ -19,168 +19,107 @@ resource "aws_key_pair" "generated_key" {
   key_name   = var.EC2_Key
   public_key = tls_private_key.mynewkey.public_key_openssh
 }
-
-// security group :
-
-resource "aws_security_group" "mysg" {
-
-depends_on = [
-    aws_vpc.ownvpc,
-  ]
-
-  name         = "allow_http"
-  description  = "Allow http inbound traffic"
-  vpc_id       = aws_vpc.ownvpc.id
- 
-  ingress {
-    description = "SSH Port"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "http from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "efs mount"
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  } 
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "myweb_sg"
-  }
-}
-
-// creation of vpc :
-resource "aws_vpc" "ownvpc" {
-  cidr_block = "192.168.0.0/16"
+resource "aws_vpc" "my-vpc" {
+  cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "addy_vpc"
-  }
+  enable_dns_hostnames = "true"
+  
 }
 
-// creation of subnets :
-resource "aws_subnet" "mypublicsubnet" {
-  vpc_id            = "${aws_vpc.ownvpc.id}"
+resource "aws_subnet" "my-subnet" {
+  vpc_id     = "${aws_vpc.my-vpc.id}"
+  cidr_block = "192.168.0.0/24"
   availability_zone = "ap-south-1a"
-  cidr_block        = "192.168.0.0/24"
+  map_public_ip_on_launch = "true"
+
+  
+}
+resource "aws_internet_gateway" "my-ig" {
+  vpc_id = "${aws_vpc.my-vpc.id}"
 }
 
-// creation of internet gateway :
-resource "aws_internet_gateway" "mygw" {
-  vpc_id = aws_vpc.ownvpc.id
-
-  tags = {
-    Name = "addy_gw"
-  }
-}
-
-// creation of route tables :
-resource "aws_route_table" "my_route_table1" {
-  vpc_id = aws_vpc.ownvpc.id
+resource "aws_route_table" "rt_tb" {
+  vpc_id = "${aws_vpc.my-vpc.id}"
 
   route {
+    
+    gateway_id = "${aws_internet_gateway.my-ig.id}"
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.mygw.id
-  }
-
-  tags = {
-    Name = "addy_routetable"
   }
 }
 
-// associating the route tables :
-resource "aws_route_table_association" "route_table_association1" {
-  subnet_id      = aws_subnet.mypublicsubnet.id
-  route_table_id = aws_route_table.my_route_table1.id
+
+resource "aws_route_table_association" "rt_associate" {
+  subnet_id      = aws_subnet.my-subnet.id
+  route_table_id = aws_route_table.rt_tb.id
 }
-// EC2 Instance and configuring httpd,git,php,amazon-efs-utils in it :
+
+resource "aws_security_group" "my-sg" {
+vpc_id = "${aws_vpc.my-vpc.id}"
+
+ingress {
+description = "NFS"
+from_port = 2049
+to_port = 2049
+protocol = "tcp"
+cidr_blocks = [ "0.0.0.0/0" ]
+}
+
+ingress {
+description = "HTTP"
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = [ "0.0.0.0/0" ]
+}
+
+ingress {
+description = "SSH"
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = [ "0.0.0.0/0" ]
+}
+
+egress {
+from_port= 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = [ "0.0.0.0/0" ]
+}
+
+}
+
+resource "aws_efs_file_system" "my-efs"{ 
+
+}
+resource "aws_efs_mount_target" "efs-tar" {
+file_system_id = "${aws_efs_file_system.my-efs.id}"
+subnet_id = "${aws_subnet.my-subnet.id}"
+security_groups = ["${aws_security_group.my-sg.id}"]
+}
 
 resource "aws_instance" "myterraformos1" {
+ami = "ami-0447a12f28fddb066"
+instance_type = "t2.micro"
+key_name      = var.EC2_Key
+subnet_id = "${aws_subnet.my-subnet.id}"
+vpc_security_group_ids = [ "${aws_security_group.my-sg.id}" ]
 
-depends_on = [
-    aws_security_group.mysg,
-  ]
-
-  ami           = "ami-0447a12f28fddb066"
-  instance_type = "t2.micro"
-  key_name      = var.EC2_Key
-  vpc_security_group_ids = ["${aws_security_group.mysg.id}"]
-  associate_public_ip_address = true
-  subnet_id = aws_subnet.mypublicsubnet.id
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = tls_private_key.mynewkey.private_key_pem
-    host     = aws_instance.myterraformos1.public_ip
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install httpd php git amazon-efs-utils -y",
-      "sudo systemctl restart httpd",
-      "sudo systemctl enable httpd",
-    ]
-  }
-
-  tags = {
-    Name = "OSterraform"
-  }
-}
-
-// EFS volume :
-
-resource "aws_efs_file_system" "myefsvol" {
-  tags = {
-    name = "myefs"
-  }
-}
-
-// mounting EFS volume :
-
-resource "aws_efs_mount_target" "mountvol" {
-  file_system_id = "${aws_efs_file_system.myefsvol.id}"
-  subnet_id      = "${aws_subnet.mypublicsubnet.id}"
-}
-
-// deploy github code in /var/www/html :
-
-resource "null_resource" "mountingvol"  {
-
-depends_on = [
-    aws_efs_mount_target.mountvol,
-  ]
-
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = tls_private_key.mynewkey.private_key_pem
-    host     = aws_instance.myterraformos1.public_ip
-  }
-
-provisioner "remote-exec" {
-    inline = [
-      "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.myefsvol.dns_name}:/ /var/www/html",
-      "sudo su -c \"echo '${aws_efs_file_system.myefsvol.dns_name}:/ /var/www/html nfs4 defaults,vers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0' >> /etc/fstab\",",
-      "sudo git clone https://github.com/aaditya2801/terraformjob1.git /var/www/html",
-    ]
-  }
+user_data = <<-EOF
+      #! /bin/bash
+	sudo su - root
+	sudo yum install httpd -y
+        sudo service httpd start
+	sudo service httpd enable
+ 	sudo yum install git -y
+        sudo yum install -y amazon-efs-utils 
+        sudo mount -t efs "${aws_efs_file_system.my-efs.id}":/ /var/www/html
+	mkfs.ext4 /dev/sdf	
+	mount /dev/sdf /var/www/html
+	sudo git clone https://github.com/aaditya2801/terraformjob1.git /var/www/html
+	  
+EOF
 }
 
 // S3 bucket :
@@ -311,7 +250,7 @@ connection {
 
 resource "null_resource" "deploywebapp"  {
 depends_on = [
-     aws_cloudfront_distribution.CloudFrontAccess,
+     null_resource.addingurl,
   ]
 
 	provisioner "local-exec" {
